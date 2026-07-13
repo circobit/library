4aae6961cb0a234a64e91999781a730e06220d09
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-07-13 19:26:07 +0200

    add renderBookCard()

--- js/scrypt.js
+++ js/scrypt.js
@@ -62,3 +62,80 @@ function addBookToLibrary(bookObj) {
 	// Push Book to library array
 	library.push(bookToAdd);
 };
+
+
+// Function to add book card
+
+function renderBookCard(book) {
+	//=== Create elements ===//
+	// Get booksList div
+	const booksList = document.getElementById("booksList");
+	console.log(typeof booksList);
+	console.log(booksList);
+	// Add bookCard
+	const bookCard = document.createElement("div");
+	bookCard.className = "bookCard";
+	booksList.appendChild(bookCard);
+	// Add bookCardTop
+	const bookCardTop = document.createElement("div");
+	bookCardTop.className = "bookCardTop";
+	bookCard.appendChild(bookCardTop);
+	// Add breakLine
+	const breakLine = document.createElement("hr");
+	breakLine.className= "breakLine";
+	bookCard.appendChild(breakLine);
+	// Add bookCardBottom
+	const bookCardBottom = document.createElement("div");
+	bookCardBottom.className = "bookCardBottom";
+	bookCard.appendChild(bookCardBottom);
+	// Add elements in bookCardTop
+	const bookTitleAndAuthor = document.createElement("div");
+	bookTitleAndAuthor.className = "bookTitleAndAuthor";
+	const bookPagesAndStatus = document.createElement("div");
+	bookPagesAndStatus.className = "bookPagesAndStatus";
+	bookCardTop.appendChild(bookTitleAndAuthor);
+	bookCardTop.appendChild(bookPagesAndStatus);
+	// Add elements in bookTitleAndAuthor
+	const bookTitle = document.createElement("p");
+	bookTitle.className = "bookTitle";
+	const bookAuthor = document.createElement("p");
+	bookAuthor.className = "bookAuthor";
+	const bookPages = document.createElement("p");
+	bookPages.className = "bookPages";
+	bookTitleAndAuthor.appendChild(bookTitle);
+	bookTitleAndAuthor.appendChild(bookAuthor);
+	// Add elements in bookPagesAndStatus
+	const isRead = document.createElement("p");
+	isRead.className = "isRead";
+	bookPagesAndStatus.appendChild(bookPages);
+	bookPagesAndStatus.appendChild(isRead);
+	// Add elements in bookCardBottom
+	const buttonRead = document.createElement("button");
+	buttonRead.className = "readButton";
+	bookCardBottom.appendChild(buttonRead);
+	const buttonDelete = document.createElement("button");
+	buttonDelete.className = "deleteButton";
+	buttonDelete.textContent = "Delete";
+	bookCardBottom.appendChild(buttonDelete);
+	//=== Populate elements with text ===//
+	bookTitle.textContent = book.title;
+	bookAuthor.textContent = book.nameAuthor + " " + book.surnameAuthor;
+	bookPages.textContent = book.pages + " pages";
+	if (book.read === true) {
+		isRead.textContent = "Read";
+		buttonRead.textContent = "Mark as unread"
+		isRead.style.padding = "0.05rem 0.4rem";
+		isRead.style.backgroundColor = "rgb(5, 41, 5)";
+		isRead.style.border = "solid rgb(5, 41, 5)";
+		isRead.style.borderRadius = "20px";
+		isRead.style.color = "rgb(68, 164, 49)";
+	} else {
+		isRead.textContent = "Pending"
+		buttonRead.textContent = "Mark as read"
+		isRead.style.padding = "0.05rem 0.4rem";
+		isRead.style.backgroundColor = "rgb(28, 28, 28)";
+		isRead.style.border = "solid rgb(28, 28, 28)";
+		isRead.style.borderRadius = "20px";
+		isRead.style.color = "rgb(163, 162, 162)";
+	};
+};

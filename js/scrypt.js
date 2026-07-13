1589f7533102cac0874fc8b023475789b27be88d
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-07-13 12:37:23 +0200

    add Book constructor

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,3 +1,15 @@
 // Library Array
 
 const library = [];
+
+// Book constructor
+
+function Book(title, nameAuthor, surnameAuthor, pages) {
+	this.id = crypto.randomUUID();
+	this.title = title;
+	this.nameAuthor = nameAuthor;
+	this.surnameAuthor = surnameAuthor;
+	this.pages = pages;
+}
+
+// 

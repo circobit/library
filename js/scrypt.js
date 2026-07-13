ff151b610a7974141a132bdd8aad13c2861ff1e6
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-07-13 12:52:23 +0200

    add read attribute to Book constructor

--- js/scrypt.js
+++ js/scrypt.js
@@ -4,12 +4,13 @@ const library = [];
 
 // Book constructor
 
-function Book(title, nameAuthor, surnameAuthor, pages) {
+function Book(title, nameAuthor, surnameAuthor, pages, read = false) {
 	this.id = crypto.randomUUID();
 	this.title = title;
 	this.nameAuthor = nameAuthor;
 	this.surnameAuthor = surnameAuthor;
 	this.pages = pages;
-}
+	this.read = read;
+};
 
 // 
